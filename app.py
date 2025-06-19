2ae9dd5886d8659a4b485c900299d2b214e59fac
20221039@uthh.edu.mx <20221039@uthh.edu.mx>
2025-06-18 22:09:20 -0600

    modified:   app.py 	modified:   templates/index.html

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ model = joblib.load("model.pkl")
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html")  # Mostrar index.html
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    data = request.get_json()
     try:
+        data = request.get_json()
         features = np.array(data["features"]).reshape(1, -1)
         prediction = model.predict(features)
-        return jsonify({"prediction": int(prediction[0])})
+        return jsonify({"prediction": str(prediction[0])})
     except Exception as e:
         return jsonify({"error": str(e)})
 
