e41e4f55bf6911b6dbcda846d30bf3c637e018c9
20221039@uthh.edu.mx <20221039@uthh.edu.mx>
2025-06-18 21:52:13 -0600

    modified:   app.py 	renamed:    index.html -> templates/index.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import joblib
 import numpy as np
 
@@ -7,13 +7,17 @@ model = joblib.load("model.pkl")
 
 @app.route("/")
 def home():
-    return "¡Hola, Mundo desde Flask!"
+    return render_template("index.html")
 
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
-    prediction = model.predict(np.array(data["features"]).reshape(1, -1))
-    return jsonify({"prediction": int(prediction[0])})
+    try:
+        features = np.array(data["features"]).reshape(1, -1)
+        prediction = model.predict(features)
+        return jsonify({"prediction": int(prediction[0])})
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
     app.run(debug=True)
